33a2cabea68634393ad354062bd51f6e99385565
Roman Romanenko <romarom81@mail.ru>
2023-05-22 17:21:24 +0300

    add Home Task for 5lesson

---
+++ Seminar5/HTask5/catalog.java
@@ -0,0 +1,8 @@
+package Seminar5.HTask5;
+
+public class catalog extends laptop {
+
+    public catalog(int id, int ram, int ssd, String os, String color){
+        super(id,ram,ssd,os,color);
+    }
+}
